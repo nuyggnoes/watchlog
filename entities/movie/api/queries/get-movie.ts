5f48622a57003ed851fa64d84377fff20aeaad03
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2025-10-15 20:49:37 +0900

    [REFACTOR] query 함수 이름 수정

--- entities/movie/api/queries/get-movie.ts
+++ entities/movie/api/queries/get-movie.ts
@@ -2,7 +2,7 @@ import { mapTMDBMovieDetail } from "../mapper";
 import { tmdbFetch } from "../tmdb-client";
 import { TMDBMovieDetail } from "../tmdb-types";
 
-export async function getMovieById(id: string) {
+export async function getMovieDetails(id: string) {
   const data = await tmdbFetch<TMDBMovieDetail>(`/movie/${id}`, {
     language: "ko-KR",
   });
